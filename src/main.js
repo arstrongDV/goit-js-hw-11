cc20d387cb520b44d8d395cf263e7922e0be1b50
arstrong <arsendacenko89@gmail.com>
2026-02-17 16:35:38 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,41 @@
+import getImagesByQuery from './js/pixabay-api'
+import { createGallery, hideLoader, showLoader, clearGallery } from './js/render-functions';
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const form = document.querySelector(".form");
+
+const handleSubmit = (e) => {
+    
+    e.preventDefault();
+    const input = e.target.elements["search-text"];
+    const text = input.value;
+    
+    if(!text){
+        return;
+    }
+
+    clearGallery();
+    showLoader();
+
+    getImagesByQuery(text)
+    .then(res => {
+        hideLoader();
+        if(res.data.hits.length == 0){
+            iziToast.show({
+                message: 'Sorry, there are no images matching your search query. Please try again!'
+            });
+            return;
+        }
+        createGallery(res.data.hits);
+        input.value = '';
+    })
+    .catch(() => {
+        hideLoader();
+        iziToast.show({
+            message: 'Sorry, there are no images matching your search query. Please try again!'
+        });
+    });
+}
+
+form.addEventListener("submit", handleSubmit);
